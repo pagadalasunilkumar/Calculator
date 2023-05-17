0d98e5180c0eface275275631e7616bfd5b34f7b
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-17 10:23:55 +0530

    Substraction

---
+++ substraction.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace Calculator
+{
+    public class Subtraction
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+            int subtract = x - y;
+            Console.WriteLine(subtract);
+        }
+    }
+}
