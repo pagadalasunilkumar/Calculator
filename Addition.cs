f52ab78c00b9934aa8b7e8233e764a408e3cca1b
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-17 10:05:41 +0530

    Add: Addition

---
+++ Addition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Addition
+{
+    public class Addition
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+            int sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
